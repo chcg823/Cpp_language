6e962468968e11a2a1696ffe04c57293fbf475f8
Chittajallu Chinmai Ganesh <46193776+chcg823@users.noreply.github.com>
2020-05-26 12:16:31 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main()
+{
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    cout<<"Factorial of "<<n<<" is "<<factorial(n);
+    return 0;
+}
+
+int factorial(int n)
+{
+    if(n!=0)
+    {
+        return n*factorial(n-1);
+    }
+    else
+    {
+        return 1;
+    }
+}
